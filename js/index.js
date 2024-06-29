4dcce2d61e63dd9da0caa7f7aa88bb425835fbed
AhmadjonNoyibjonov <ahmadjonnoyibjonov60@gmail.com>
2024-06-29 23:55:22 +0500

    project

---
+++ js/index.js
@@ -0,0 +1,56 @@
+const field = document.querySelector("#field");
+const button = document.querySelector("button");
+const wrapper = document.querySelector("#todo-wrapper");
+
+function createItem(value) {
+  return `
+          <div class="item">
+                    <div class="left">
+                         <input type="checkbox" name="" id="">
+                         <p>${value}</p>
+                    </div>
+
+                    <div class="right">
+                         <button>
+                              <i class="fa-regular fa-pen-to-square"></i>
+                              <span>Edit</span>
+                         </button>
+
+                         <button>
+                              <i class="fa-solid fa-trash-can"></i>
+                              <span>Del</span>
+                         </button>
+
+                    </div>
+               </div>
+     `;
+}
+
+// function validate() {
+// const todo = field.value
+
+//     if(todo.length < 5) {
+//         alert("Eng kamida 6 ta belgidan iborat bo'lishi kerak")
+//         field.focus()
+//         field.style.outlineColor = "red"
+//         return false
+//     }
+// }
+
+button &&
+  button.addEventListener("click", function (event) {
+    event.preventDefault();
+    const todo = field.value;
+
+    if (todo.length < 5) {
+      alert("Eng kamida 6 ta belgidan iborat bo'lishi kerak");
+      field.focus();
+      field.style.outlineColor = "red";
+      return false;
+    }
+
+    const item = createItem(todo);
+    wrapper.innerHTML += item;
+    field.value = "";
+    field.focus();
+  });
